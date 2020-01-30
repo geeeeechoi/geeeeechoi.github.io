2d624624b239894ec184647cb9b67f6040501fc0
geeeeechoi <60239489+geeeeechoi@users.noreply.github.com>
2020-01-30 10:42:06 -0600

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,9 +1,13 @@
 let r = 0;
 let g = 0;
 let b = 255;
+let triY;
+let triX;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  triY = 700;
+  triX = 360;
 }
 
 function draw() {
@@ -11,11 +15,12 @@ function draw() {
   b = map (mouseX, 0, windowWidth, 255, 0);
   r = map (mouseY, 0, windowHeight, 0, 255);
   background(r,g,b);
+  
 
 
 
   fill(200);
-  triangle(18, 18, 18, 360, 81, 360)
+  triangle(18, 18, 18, triX, 81, 360)
 
   fill(200);
   triangle(18, 18, 18, 360, 81, 360)
@@ -34,11 +39,26 @@ function draw() {
   ellipse(mouseX, mouseY, 64, 64);
 
   fill(200)
-  triangle(800,18, 600,700, 800, 600);
+  triangle(800,18, 600,triY, 800, 600);
 
   fill(240);
   arc(479, 300, 280, 280, PI, TWO_PI);
 
+  if(mouseIsPressed){
+    //when mouse is pressed reset pointx to 200
+    triY = 600;
+  } else {
+    //when mouse is released reset pointx to 600
+    triY = 700;
+  }
+  
+   if(mouseIsPressed){
+    //when mouse is pressed reset pointx to 200
+    triX = 500;
+  } else {
+    //when mouse is released reset pointx to 600
+    triX = 360;
+  }
 
 
 }
